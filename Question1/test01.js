3e15c07a8705c4d6702aa55229bb30d1e86c1b02
allanisjoiceelectraj.sumaya <allanissumaya22@gmail.com>
2023-10-05 11:30:27 -0400

    Edited

--- Question1/test01.js
+++ Question1/test01.js
@@ -23,36 +23,3 @@ lowerCaseWords(mixedArray)
         console.error("Error:", error);
     });
 
-
-// Question 2:
-const resolvedPromise = () => {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve({ message: 'delayed success!' });
-        }, 500);
-    });
-};
-
-const rejectedPromise = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            reject(new Error('error: delayed exception!'));
-        }, 500);
-    });
-};
-
-resolvedPromise()
-    .then((result) => {
-        console.log(result);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
-
-rejectedPromise()
-    .then((result) => {
-        console.log(result);
-    })
-    .catch((error) => {
-        console.error(error);
-    });
---
+++ Question2/test02.js
@@ -0,0 +1,32 @@
+// Question 2:
+const resolvedPromise = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve({ message: 'delayed success!' });
+        }, 500);
+    });
+};
+
+const rejectedPromise = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error('error: delayed exception!'));
+        }, 500);
+    });
+};
+
+resolvedPromise()
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+
+rejectedPromise()
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
